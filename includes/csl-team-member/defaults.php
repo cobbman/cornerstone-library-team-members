4bc3cfaba8b55e56c3e2b080fae1f37c9aea2126
William <hello@bigwilliam.com>
2016-02-26 17:42:37 -0700

    move spaces

--- includes/csl-team-member/defaults.php
+++ includes/csl-team-member/defaults.php
@@ -5,13 +5,13 @@
  */
 
 return array(
-	'id'              => '',
-	'class'           => '',
-	'style'           => '',
-	'name'            => '',
-	'job_title'       => '',
-	'highlight_color' => '#000000',
-	'member_image'    => 'http://placehold.it/200x250&text=Team+Member',
+	'id'               => '',
+	'class'            => '',
+	'style'            => '',
+	'name'             => '',
+	'job_title'        => '',
+	'highlight_color'  => '#000000',
+	'member_image'     => 'http://placehold.it/200x250&text=Team+Member',
 	'custom_open_text' => '',
-	'member_content'  => ''
+	'member_content'   => ''
 );
